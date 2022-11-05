ac71b5781aa1b77d80d153d2db6212bb2e8eba1d
kittenmasque <volaju@gmail.com>
2022-11-05 15:01:30 -0700

    updated server.js to add cacheController requirement

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-//dependencies
+// dependencies
 const express = require("express");
 require('dotenv').config({});
+const cors = require("cors");
 const app = express();
-const cors = require('cors');
-
 const mongoose = require('mongoose');
+
+// middleware
+app.use(express.json());
+app.use(cors());
+app.use(require("./middleware/logger"));
 
 
 // mongoose connection
@@ -13,19 +17,18 @@ mongoose.connect(process.env.DATABASE_URL, {
     useUnifiedTopology: true
 });
 
+
+const cacheController = require('./controllers/cache_controllers.js');
+app.use('/cache', cacheController);
+
+
 const db = mongoose.connection;
 
 db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
 db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
-//cors
-app.use(cors({
-    header: 'Access-Control-Allow-Origin'
-}));
-
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`The server is listening on port: ${PORT}`)
 });
